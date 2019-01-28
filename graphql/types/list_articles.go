cd99f6d3b0612aeef0d89b69603e14662c56b1fb
rubeniskov <me@rubeniskov.com>
2019-01-28 14:07:20 +0100

    style: clean commented code

--- graphql/types/list_articles.go
+++ graphql/types/list_articles.go
@@ -43,23 +43,3 @@ var FieldListArticles = &graphql.Field{
 		return utils.ConnectionFromArraySlice(articles, connArgs, meta), err
 	}),
 }
-
-//
-//
-// Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-//   args := relay.NewConnectionArguments(params.Args)
-//   dbModel := params.Context.Value("model").(model.Models)
-//   orders, err := dbModel.Order.ListOrders(params.Args)
-//   return relay.ConnectionFromArraySlice(orders, args, relay.ArraySliceMetaInfo{
-//     SliceStart:  0,
-//     ArrayLength: 100,
-//   }), err
-// },
-//
-// Type:        graphql.NewList(qltype.ArticleConnection),
-// Description: "Get product list",
-// Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-//   dbModel := params.Context.Value("model").(model.Models)
-//   articles, err := dbModel.Article.ListArticles()
-//   return articles, err
-// },
--- graphql/types/list_orders.go
+++ graphql/types/list_orders.go
@@ -18,27 +18,3 @@ var FieldListOrders = &graphql.Field{
 		return utils.ConnectionFromArraySlice(orders, connArgs, meta), err
 	}),
 }
-
-// import (
-// 	"github.com/globalsign/mgo/bson"
-// 	"github.com/graphql-go/graphql"
-// 	"github.com/graphql-go/relay"
-// 	"github.com/jal88/elrincondalba-ms/graphql/types"
-// )
-//
-// var ListOrders = &graphql.Field{
-// 	Type:        types.TypeOrderConnection,
-// 	Description: "Get orders list",
-// 	Args:        relay.ConnectionArgs,
-// 	Resolve: decs.ContextModelConsumer(func(params graphql.ResolveParams, model inte.Model) (interface{}, error) {
-// 		args := relay.NewConnectionArguments(params.Args)
-// 		orders, err := model.Order().FindWithPagination(params.Args, bson.M{})
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		return relay.ConnectionFromArraySlice(orders, args, relay.ArraySliceMetaInfo{
-// 			SliceStart:  0,
-// 			ArrayLength: 100,
-// 		}), err
-// 	}),
-// }
