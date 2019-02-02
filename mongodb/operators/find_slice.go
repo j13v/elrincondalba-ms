1b26dac4acca2a751b0fffdb2e4aa77849f1e99a
rubeniskov <me@rubeniskov.com>
2019-02-02 15:49:46 +0100

    feat: implement mongodb operators

---
+++ mongodb/operators/find_slice.go
@@ -0,0 +1,64 @@
+package operators
+
+import (
+	"context"
+	"log"
+
+	"github.com/jal88/elrincondalba-ms/mongodb/helpers"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
+)
+
+/*
+FindSliceMetadata
+*/
+type FindSliceMetadata struct {
+	Total      int `json:"total"`
+	SliceCount int `json:"sliceCount"`
+	SliceStart int `json:"sliceStart"`
+}
+
+func FindSlice(coll *mongo.Collection, ctx context.Context, args *map[string]interface{}) ([]bson.Raw, *FindSliceMetadata, error) {
+	filter, err := helpers.NewFindFilterFromArgs(args)
+	if err != nil {
+		return nil, nil, err
+	}
+	count, err := coll.Count(ctx, bson.D{})
+	if err != nil {
+		return nil, nil, err
+	}
+	fopts := helpers.NewFindOptionsFromArgs(*args, count)
+	cursor, err := coll.Find(ctx, filter, fopts)
+	if err != nil {
+		log.Fatal(err)
+		return nil, nil, err
+	}
+	defer cursor.Close(ctx)
+	data := []bson.Raw{}
+	for cursor.Next(ctx) {
+		item := bson.Raw{}
+		if err = cursor.Decode(&item); err != nil {
+			log.Fatal(err)
+			return nil, nil, err
+		}
+		data = append(data, item)
+	}
+	var start int64
+	if len(data) > 0 {
+		oid, err := helpers.GetObjectIDFromValue(data[0])
+		if err != nil {
+			return nil, nil, err
+		}
+		index, err := GetIndexById(coll, ctx, oid)
+		if err != nil {
+			return nil, nil, err
+		}
+		start = index
+	}
+	scount := *fopts.Limit
+	return data, &FindSliceMetadata{
+		Total:      int(count),
+		SliceCount: int(scount),
+		SliceStart: int(start),
+	}, nil
+}
---
+++ mongodb/operators/sync.go
@@ -0,0 +1,26 @@
+package operators
+
+/*
+Sync synchronize a document in the collection using an struct
+*/import (
+	"context"
+	"github.com/jal88/elrincondalba-ms/mongodb/helpers"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
+)
+
+func Sync(coll *mongo.Collection, ctx context.Context, mixed interface{}) error {
+	oid, err := helpers.GetObjectIDFromValue(mixed)
+	if err != nil {
+		return err
+	}
+	if _, err := coll.UpdateOne(
+		context.Background(),
+		bson.M{"_id": oid},
+		bson.M{
+			"$set": mixed,
+		}); err != nil {
+		return err
+	}
+	return nil
+}
